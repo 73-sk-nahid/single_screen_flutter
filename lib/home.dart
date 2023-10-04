61c9e1675681672d135ee3a1b8d90bbd374dbb92
Sk. Nahid <nahidsheikh2001@gmail.com>
2023-10-04 12:12:01 +0600

    add popular diet model

--- lib/home.dart
+++ lib/home.dart
@@ -2,28 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'models/category_model.dart';
 import 'models/diet_model.dart';
+import 'models/popular_model.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
   List<CategoryModel> categories = [];
   List<DietModel> diets = [];
+  List<PopularDietsModel> popularDiets = [];
 
-  void _getcategories() {
+  void _getInitialInfo() {
     categories = CategoryModel.getCategories();
-  }
-
-  void _getDiets() {
     diets = DietModel.getDiets();
+    popularDiets = PopularDietsModel.getPopularDiets();
   }
 
   @override
   Widget build(BuildContext context) {
-    _getcategories();
-    _getDiets();
+    _getInitialInfo();
     return Scaffold(
       appBar: appBar(),
       backgroundColor: Colors.white,
-      body: Column(
+      body: ListView(
         //crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           _searchField(),
@@ -35,6 +34,84 @@ class HomePage extends StatelessWidget {
             height: 40,
           ),
           _DietSection(),
+          SizedBox(height: 40),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Text(
+                  'Popular',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 18,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              SizedBox(height: 15,),
+              ListView.separated(
+                itemCount: popularDiets.length,
+                shrinkWrap: true,
+                separatorBuilder: (context, indext) => SizedBox(height: 25,),
+                padding: EdgeInsets.only(
+                  left: 20,
+                  right: 20
+                ),
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 110,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SvgPicture.asset(popularDiets[index].iconPath,
+                        width: 65, height: 65,),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              popularDiets[index].name,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black,
+                                fontSize: 16,
+                              ),
+                            ),
+                            Text(
+                              popularDiets[index].level +
+                                  ' | ' +
+                                  popularDiets[index].duration +
+                                  ' | ' +
+                                  popularDiets[index].calorie,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                color: Color(0xff7B6F72),
+                                fontSize: 13,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(16),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0xff1D1617).withOpacity(0.07),
+                          offset: Offset(0, 10),
+                          blurRadius: 40,
+                          spreadRadius: 0,
+                        )
+                      ]
+                    ),
+                  );
+                },
+              )
+            ],
+          ),
+          SizedBox(height: 40,),
         ],
       ),
     );
